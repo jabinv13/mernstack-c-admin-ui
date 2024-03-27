ecca5a81c618bfa76aa73221d533f7ff0e888b4c
jabin-v <jabinv03@gmail.com>
2024-03-27 17:05:12 +0530

    add sidebar layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,17 +1,82 @@
-import { Link, Navigate, Outlet } from "react-router-dom";
+import { NavLink, Navigate, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store";
+import Icon from "@ant-design/icons";
+
+import { Layout, Menu, theme } from "antd";
+
+import Sider from "antd/es/layout/Sider";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import { useState } from "react";
+import Logo from "../components/icons/Logo";
+import Home from "../components/icons/Home";
+import BasketIcon from "../components/icons/BasketIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+import UserIcon from "../components/icons/UserIcon";
+
+const items = [
+  {
+    key: "/",
+    icon: <Icon component={Home} />,
+    label: <NavLink to="/">Home</NavLink>,
+  },
+
+  {
+    key: "/promos",
+    icon: <Icon component={GiftIcon} />,
+    label: <NavLink to="/promos">Promos</NavLink>,
+  },
+  {
+    key: "/products",
+    icon: <Icon component={BasketIcon} />,
+    label: <NavLink to="/products">Products</NavLink>,
+  },
+  {
+    key: "/users",
+    icon: <Icon component={UserIcon} />,
+    label: <NavLink to="/users">Users</NavLink>,
+  },
+];
 
 export const Dashboard = () => {
   const { user } = useAuthStore();
 
+  const [collapsed, setCollapsed] = useState(false);
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   if (user === null) {
     return <Navigate to="/auth/login" replace={true} />;
   }
+
   return (
     <div>
-      <h1>Auth component</h1>
-      <Link to={"/auth/login"}>Go to Homepage</Link>
-      <Outlet />
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          theme="light"
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="logo">
+            <Logo />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Mernspace pizza shop</Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 };
--- src/layouts/NonAuth.tsx
+++ src/layouts/NonAuth.tsx
@@ -9,7 +9,6 @@ const NonAuth = () => {
   }
   return (
     <div>
-      <h1>Non Auth component</h1>
       <Outlet />
     </div>
   );
