b9cbf57e35b23712d98353c569634fc29dc6cba2
jabin-v <jabinv03@gmail.com>
2024-04-01 17:28:07 +0530

    add createUser form

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -1,6 +1,17 @@
-import { Card, Col, Form, Input, Row, Space } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Card, Col, Form, Input, Row, Select, Space } from "antd";
+import { getTenants } from "../../../http/api";
+import { Tenant } from "../../../types";
 
 const UserForm = () => {
+  const selectedRole = Form.useWatch("role");
+
+  const { data: tenants } = useQuery({
+    queryKey: ["tenants"],
+    queryFn: () => {
+      return getTenants().then((res) => res.data);
+    },
+  });
   return (
     <Row>
       <Col>
@@ -53,6 +64,80 @@ const UserForm = () => {
                   <Input size="large" />
                 </Form.Item>
               </Col>
+            </Row>
+          </Card>
+          <Card title="Security info" bordered={false}>
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item
+                  label="Passoword"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Password required",
+                    },
+                  ]}
+                >
+                  <Input size="large" type="password" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+          <Card title="Role" bordered={false}>
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item
+                  label="Role"
+                  name="role"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Role is required",
+                    },
+                  ]}
+                >
+                  <Select
+                    id="selectBoxInUserForm"
+                    size="large"
+                    style={{ width: "100%" }}
+                    allowClear={true}
+                    onChange={() => {}}
+                    placeholder="Select role"
+                  >
+                    <Select.Option value="admin">Admin</Select.Option>
+                    <Select.Option value="manager">Manager</Select.Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+              {selectedRole === "manager" && (
+                <Col span={12}>
+                  <Form.Item
+                    label="Restaurant"
+                    name="tenantId"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Restaurant is required",
+                      },
+                    ]}
+                  >
+                    <Select
+                      size="large"
+                      style={{ width: "100%" }}
+                      allowClear={true}
+                      onChange={() => {}}
+                      placeholder="Select restaurant"
+                    >
+                      {tenants?.map((tenant: Tenant) => (
+                        <Select.Option value={tenant.id} key={tenant.id}>
+                          {tenant.name}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+              )}
             </Row>
           </Card>
         </Space>
